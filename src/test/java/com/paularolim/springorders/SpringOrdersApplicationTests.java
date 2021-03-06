32c903b6983d08d6a6583f04e59d5f49851a0227
Paula Rolim <paula.oliveira124@etec.sp.gov.br>
2021-03-06 09:50:16 -0300

    init project

---
+++ src/test/java/com/paularolim/springorders/SpringOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paularolim.springorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
