5ac04d77109619a6d932cf723f49cece775f149e
Paula Rolim <paula.oliveira124@etec.sp.gov.br>
2021-03-06 11:47:41 -0300

    service layer

--- src/main/java/com/paularolim/springorders/resources/UserResource.java
+++ src/main/java/com/paularolim/springorders/resources/UserResource.java
@@ -1,17 +1,33 @@
 package com.paularolim.springorders.resources;
 
 import com.paularolim.springorders.entities.User;
+import com.paularolim.springorders.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Paula", "paula@email.com", "11999999999", "secret");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
 
         return ResponseEntity.ok().body(user);
     }
